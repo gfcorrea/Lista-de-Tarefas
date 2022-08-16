ffd60973d1635c3d3b2c9a302a025d59f555383e
Gabriel <gfcorrea01@gmail.com>
2022-08-16 15:02:52 -0300

    Refatorando

--- app/src/main/java/com/gfcorrea/listadetarefas/MainActivity.java
+++ app/src/main/java/com/gfcorrea/listadetarefas/MainActivity.java
@@ -2,7 +2,6 @@ package com.gfcorrea.listadetarefas;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -31,29 +30,14 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigationResumo
         ).build();
 
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.fragmentContainerView);
-        NavController navController = navHostFragment.getNavController();
 
-       // NavController navController = Navigation.findNavController(this, R.id.fragmentContainerView);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+
+        NavController navController = navHostFragment.getNavController();
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
-
-        /*
-
-
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_dashboard , R.id.navigation_home, R.id.navigation_notifications)
-                .build();
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);*/
 
     }
 
