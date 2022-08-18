d4a2d8e077ebd4509a89bd9b083639099ec782aa
Gabriel <gfcorrea01@gmail.com>
2022-08-18 10:05:49 -0300

    Versão 1.0

--- app/src/main/java/com/gfcorrea/listadetarefas/fragments/ResumoFragment.java
+++ app/src/main/java/com/gfcorrea/listadetarefas/fragments/ResumoFragment.java
@@ -1,5 +1,6 @@
 package com.gfcorrea.listadetarefas.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,6 +11,16 @@ import android.view.ViewGroup;
 
 import com.gfcorrea.listadetarefas.R;
 import com.gfcorrea.listadetarefas.databinding.FragmentResumoBinding;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ResumoFragment extends Fragment {
@@ -30,6 +41,50 @@ public class ResumoFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentResumoBinding.inflate(getLayoutInflater());
 
+        setupPieChart();
+        loadPieData();
+
         return binding.getRoot();
+    }
+
+    public void setupPieChart(){
+        binding.pieChartResumo.setDrawHoleEnabled(true);
+        binding.pieChartResumo.setUsePercentValues(true);
+        binding.pieChartResumo.setEntryLabelTextSize(12);
+        binding.pieChartResumo.setEntryLabelColor(Color.BLACK);
+        binding.pieChartResumo.setCenterText("Resumo das Tarefas");
+        binding.pieChartResumo.setCenterTextSize(24);
+        binding.pieChartResumo.getDescription().setEnabled(false);
+
+        Legend l = new Legend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setEnabled(true);
+    }
+
+    public void loadPieData(){
+        List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(0.33f, "Ativos"));
+        entries.add(new PieEntry(0.33f, "Concluídos"));
+        entries.add(new PieEntry(0.33f, "Atrasados"));
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for(int color: ColorTemplate.MATERIAL_COLORS){
+            colors.add(color);
+        }
+
+        PieDataSet dataset = new PieDataSet(entries, "");
+        dataset.setColors(colors);
+
+        PieData data = new PieData(dataset);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(binding.pieChartResumo));
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.BLACK);
+
+        binding.pieChartResumo.setData(data);
+        binding.pieChartResumo.invalidate();
     }
 }
