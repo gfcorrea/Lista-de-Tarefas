224b2a7d8b6455f26e2643e56497985234d4f751
Gabriel <gfcorrea01@gmail.com>
2022-08-20 17:25:22 -0300

    Versão 1.0

--- app/src/main/java/com/gfcorrea/listadetarefas/fragments/TarefaFragment.java
+++ app/src/main/java/com/gfcorrea/listadetarefas/fragments/TarefaFragment.java
@@ -1,17 +1,21 @@
 package com.gfcorrea.listadetarefas.fragments;
 
 import android.annotation.SuppressLint;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.gfcorrea.listadetarefas.MainActivity;
 import com.gfcorrea.listadetarefas.R;
 import com.gfcorrea.listadetarefas.database.TarefaModel;
 import com.gfcorrea.listadetarefas.databinding.FragmentTarefaBinding;
@@ -19,7 +23,9 @@ import com.gfcorrea.listadetarefas.repository.TarefaRepository;
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
+
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 
@@ -27,6 +33,7 @@ public class TarefaFragment extends Fragment {
     private FragmentTarefaBinding binding;
     private final TarefaRepository tarefaRepository = new TarefaRepository();
     private long dataSelecionada = 0;
+    private int horaSelecionada = 0, minutoSelecionado = 0;
 
     public TarefaFragment() {
         // Required empty public constructor
@@ -63,7 +70,36 @@ public class TarefaFragment extends Fragment {
             }
         });
 
+        binding.tvHoraTarefa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowTimeDialog();
+            }
+        });
+
+
         return binding.getRoot();
+    }
+
+    private void ShowTimeDialog() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                getContext(),
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourSelect, int minuteSelect) {
+                        horaSelecionada = hourSelect;
+                        minutoSelecionado = minuteSelect;
+
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(0, 0, 0, hourSelect, minuteSelect);
+
+                        binding.tvHoraTarefa.setText(DateFormat.format("hh:mm aa", calendar));
+                    }
+                }
+                , 12, 0, false);
+
+        timePickerDialog.updateTime(horaSelecionada, minutoSelecionado);
+        timePickerDialog.show();
     }
 
     private void ShowDateDialog() {
@@ -74,11 +110,9 @@ public class TarefaFragment extends Fragment {
             @SuppressLint("SetTextI18n")
             @Override
             public void onPositiveButtonClick(Object selection) {
+                dataSelecionada = materialDatePicker.getSelection();
 
-                String longV = materialDatePicker.getSelection().toString();
-                dataSelecionada = Long.parseLong(longV);
-
-                String dateString = new SimpleDateFormat("dd/MM/yyyy").format( new Date(dataSelecionada) );
+                String dateString = new SimpleDateFormat("dd/MM/yyyy").format(new Date(dataSelecionada));
                 binding.tvDataTarefa.setText(dateString);
             }
         });
---
+++ app/src/main/java/com/gfcorrea/listadetarefas/viewmodel/TarefaViewModel.java
@@ -0,0 +1,22 @@
+package com.gfcorrea.listadetarefas.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+import com.gfcorrea.listadetarefas.database.TarefaModel;
+
+public class TarefaViewModel extends ViewModel {
+
+    private TarefaModel tarefa = new TarefaModel();
+    private long dataSelecionada = 0;
+
+    public long getDataSelecionada() {
+        return dataSelecionada;
+    }
+
+    public void setDataSelecionada(long dataSelecionada) {
+        this.dataSelecionada = dataSelecionada;
+    }
+
+
+
+}
